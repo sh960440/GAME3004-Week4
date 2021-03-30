e5b305981a3e81c5ba962640ea0fee3005416603
sh960440 <54958110+sh960440@users.noreply.github.com>
2021-03-29 22:59:11 -0400

    Added enemy prefab

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -1,22 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.AI;
 
+[System.Serializable]
+public enum EnemyState
+{
+    IDLE,
+    RUN,
+    JUMP,
+    PUNCH,
+    DIE
+}
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
 public class EnemyBehavior : MonoBehaviour
 {
     public NavMeshAgent navMeshAgent;
-    public Transform player;
+    public PlayerBehavior player;
+    public Animator controller;
 
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        player = FindObjectOfType<PlayerBehavior>();
+        controller = GetComponent<Animator>();
+
+        controller.SetInteger("AnimState", (int)EnemyState.RUN);
     }
 
     // Update is called once per frame
     void Update()
     {
-        navMeshAgent.SetDestination(player.position);
+        if (player)
+        {
+            navMeshAgent.SetDestination(player.transform.position);
+        }
     }
 }
